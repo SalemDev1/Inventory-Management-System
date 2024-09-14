466286c341004f2392654ad301cec96fb83a0a0c
Mamun Salem <salemtwin2@gmail.com>
2024-09-14 16:10:49 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,10 @@
+//Inventory Management System
+
+// Task 1- Create an Inventory Array of Product Objects
+const inventory= [
+    { name: 'Gaming PC', price: 1200, quantity: 10, lowStockLevel: 3 },
+    { name: 'Valorant', price: 50, quantity: 5, lowStockLevel: 2 },
+    { name: 'GTA VI', price: 500, quantity: 7, lowStockLevel: 1 },
+    { name: 'Caniac Combo', price: 16, quantity: 15, lowStockLevel: 5 },
+    { name: 'Stardew Valley', price: 250, quantity: 3, lowStockLevel: 1 }
+];
