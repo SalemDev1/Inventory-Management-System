0daed354cc6e92ec3c1549f6f0b73eddcac8461f
Mamun Salem <salemtwin2@gmail.com>
2024-09-14 17:25:45 -0400

    Create updateStock function

--- inventory_management.js
+++ inventory_management.js
@@ -9,10 +9,10 @@ const inventory= [
     { name: 'Stardew Valley', price: 250, quantity: 3, lowStockLevel: 1 }
 ];
 
-//Task 2- Create a Function to Display Product Details
+// Task 2- Create a Function to Display Product Details
 function displayProductDetails(products) {
     for(let i=0;i<products.length;i++){ 
-        if (products[i].lowStockLevel< 3) {
+        if (products[i].lowStockLevel == products[i].quantity) {
             console.log(`Product: ${products[i].name}, Price: ${products[i].price}, Quantity: ${products[i].quantity}, Status: Low Stock `);
         }
         else {
@@ -22,3 +22,16 @@ function displayProductDetails(products) {
 }
 displayProductDetails(inventory);
 // Outputs the product’s name, price, quantity in stock, and stock status
+
+// Task 3- Create a Function to Update Product Stock After Sales
+function updateStock (products, unitsSold) {
+products.quantity -= unitsSold;
+if (products.lowStockLevel == products.quantity){
+    console.log (`${products.name} is now Low Stock`);
+}
+else if (products.quantity == 0) {
+    console.log (`${products.name} is now Out of Stock`);
+}
+};
+updateStock(inventory[0],7);
+// Outputs "Gaming PC is now Low Stock as the quanity changed 3"
