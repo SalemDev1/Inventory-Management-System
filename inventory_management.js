8793ce5f068a2e39277cc4bf50563f7c2e126183
Mamun Salem <salemtwin2@gmail.com>
2024-09-14 20:51:52 -0400

    Create processSale function

--- inventory_management.js
+++ inventory_management.js
@@ -11,29 +11,31 @@ const inventory= [
 
 // Task 2- Create a Function to Display Product Details
 function displayProductDetails(products) {
-    for(let i=0;i<products.length;i++){ 
-        if (products[i].lowStockLevel == products[i].quantity) {
-            console.log(`Product: ${products[i].name}, Price: ${products[i].price}, Quantity: ${products[i].quantity}, Status: Low Stock `);
-        }
-        else {
-            console.log(`Product: ${products[i].name}, Price: ${products[i].price}, Quantity: ${products[i].quantity}, Status: In Stock `);
-        }   
+    if (products.lowStockLevel >= products.quantity) {
+        console.log(`Product: ${products.name}, Price: ${products.price}, Quantity: ${products.quantity}, Status: Low Stock `);
     }
-}
-displayProductDetails(inventory);
+    else {
+        console.log(`Product: ${products.name}, Price: ${products.price}, Quantity: ${products.quantity}, Status: In Stock `);
+    }
+  }
+  for(let i=0;i<inventory.length;i++){ 
+    displayProductDetails(inventory[i]);
+  }
+
 // Outputs the product’s name, price, quantity in stock, and stock status
 
 // Task 3- Create a Function to Update Product Stock After Sales
 function updateStock (products, unitsSold) {
 products.quantity -= unitsSold;
-if (products.lowStockLevel == products.quantity){
+if (products.lowStockLevel >= products.quantity){
     console.log (`${products.name} is now Low Stock`);
 }
-else if (products.quantity == 0) {
+else if (products.quantity <= 0) {
     console.log (`${products.name} is now Out of Stock`);
 }
 };
 updateStock(inventory[0],7);
+displayProductDetails(inventory[0]);
 // Outputs "Gaming PC is now Low Stock as the quanity changed 3"
 
 // Task 4- Create a Function to Check Low Stock Products
@@ -58,3 +60,22 @@ function calculateInventoryValue(products) {
     return totalValue;
 }
 console.log("Total Inventory Value: $"+ calculateInventoryValue(inventory));
+// Outputs- Total Inventory Value: $8180
+
+//Task 6- Create a Function to Process a Sale
+function processSale(productName,soldUnits,products) {
+    let index = 0;
+    let found = products.find(function(element){
+        index = products.indexOf(element);
+        return element.name === productName; 
+    });
+    if (found)  {
+        updateStock(products[index], soldUnits);
+    }
+else {
+    console.error ("ERROR: " + productName + " not found");
+}
+}
+processSale('Gaming PC', 2, inventory);
+displayProductDetails(inventory[0]);
+//Outputs "Product: Gaming PC, Price: 1200, Quantity: 1, Status: Low Stock "
